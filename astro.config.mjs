57b411a61106b0c5eeda8fb1827b9fc5dff514ba
Marvin <marvinbarretto@gmail.com>
2024-12-14 23:54:40 +0530

    Update pacakges and add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sanity from '@sanity/astro';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server', 
+    output: 'server',
+    adapter: vercel(),
     integrations: [
         sanity({
             projectId: "8cxh2o2e",
