2732f98474b922e5754bd56d7a01f2f08ff6d3c8
Marvin <marvinbarretto@gmail.com>
2025-05-14 16:10:13 +0100

    Clean up Distaff page

--- src/utils/sanity.ts
+++ src/utils/sanity.ts
@@ -122,6 +122,35 @@ export async function fetchAllPublications(): Promise<Publication[]> {
   return await getSanityData<Publication[]>(query);
 }
 
+export async function fetchLatestPublication(): Promise<Publication | null> {
+  const query = `
+    *[_type == "publication"] | order(_createdAt desc)[0] {
+      _id,
+      title,
+      description,
+      "documentUrl": document.asset->url
+    }
+  `;
+  return await getSanityData<Publication>(query);
+}
+
+export async function fetchArchivePublications(): Promise<Publication[]> {
+  const query = `
+    *[_type == "publication"] | order(_createdAt desc)[1..9999] {
+      _id,
+      title,
+      description,
+      "documentUrl": document.asset->url
+    }
+  `;
+  return await getSanityData<Publication[]>(query);
+}
+
+
+
+
+
+
 export async function fetchPostsPage(): Promise<Post[]> {
   const posts = `
     *[_type == "post" && defined(slug.current)]
