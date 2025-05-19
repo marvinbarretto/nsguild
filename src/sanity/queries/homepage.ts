b7db9da549fd28794b8b0aec702a70f50f979490
Marvin <marvinbarretto@gmail.com>
2025-05-19 15:24:35 +0100

    refactor: PortableText doesnt need wrapper and simplify homepage null checks

--- src/sanity/queries/homepage.ts
+++ src/sanity/queries/homepage.ts
@@ -1,37 +1,42 @@
 import { getSanityData } from "../../utils/sanity";
 import type { Homepage } from "../../utils/types";
 import type { Banner } from "../../utils/types";
-export async function fetchHomepage(): Promise<Homepage | null> {
-    const query = `
-      *[_type == "homepage"][0] {
-        banner {
-          isActive,
-          message,
-          link
-        },
-carouselImages[]{
-      "asset": asset->{
-        _id,
-        url
-      }
-    },
-        whoWeAre,
-        whatWeDo,
-        whenWeMeet,
-        whereWeMeet,
-        description
-      }
-    `;
-    return await getSanityData<Homepage | null>(query);
+
+export async function fetchHomepage(): Promise<Homepage> {
+  const query = `
+    *[_type == "homepage"][0] {
+      banner {
+        isActive,
+        message,
+        link
+      },
+      carouselImages[]{
+        "asset": asset->{
+          _id,
+          url
+        }
+      },
+      whoWeAre,
+      whatWeDo,
+      whenWeMeet,
+      whereWeMeet,
+      description
+    }
+  `;
+  const data = await getSanityData<Homepage | null>(query);
+  if (!data) throw new Error("Sanity: Homepage not found");
+  return data;
 }
 
-export async function fetchBanner(): Promise<Banner | null> {
-    const query = `
-      *[_type == "banner"][0] {
-        message,
-        isActive,
-        link
-      }
-    `;
-    return await getSanityData(query);
-  }
+export async function fetchBanner(): Promise<Banner> {
+  const query = `
+    *[_type == "banner"][0] {
+      message,
+      isActive,
+      link
+    }
+  `;
+  const data = await getSanityData<Banner | null>(query);
+  if (!data) throw new Error("Sanity: Banner not found");
+  return data;
+}
